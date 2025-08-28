2441f568ae5a43e09b4694166db6b2e885d7f1dc
rithika1919 <paidalarethikareddy@gmail.com>
2025-08-28 13:42:39 +0530

    back to the checkpoint

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,45 +1,20 @@
-import type { Metadata } from 'next';
-
-import Prose from 'components/prose';
 import { getPage } from 'lib/shopify';
 import { notFound } from 'next/navigation';
 
-export async function generateMetadata(props: {
-  params: Promise<{ page: string }>;
-}): Promise<Metadata> {
-  const params = await props.params;
-  const page = await getPage(params.page);
-
-  if (!page) return notFound();
-
-  return {
-    title: page.seo?.title || page.title,
-    description: page.seo?.description || page.bodySummary,
-    openGraph: {
-      publishedTime: page.createdAt,
-      modifiedTime: page.updatedAt,
-      type: 'article'
-    }
-  };
-}
-
-export default async function Page(props: { params: Promise<{ page: string }> }) {
-  const params = await props.params;
+export default async function Page({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
 
   if (!page) return notFound();
 
   return (
-    <>
-      <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
-      <Prose className="mb-8" html={page.body} />
-      <p className="text-sm italic">
-        {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric'
-        }).format(new Date(page.updatedAt))}.`}
-      </p>
-    </>
+    <div className="mx-auto py-8 sm:py-16 lg:py-20">
+      <h1 className="mb-8 text-5xl font-bold leading-tight tracking-tight text-gray-900">
+        {page.title}
+      </h1>
+      <div
+        className="prose mx-auto max-w-screen-lg"
+        dangerouslySetInnerHTML={{ __html: page.body }}
+      />
+    </div>
   );
 }
